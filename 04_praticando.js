cb59b3a3dc180a582280967ef5359369e0cd740f
Alex <alextayrom@gmail.com>
2021-03-22 22:53:10 -0300

    first commit

---
+++ 04_praticando.js
@@ -0,0 +1,55 @@
+alert("Vamos Eliminar os Brothers");
+
+let p1 = "Juliete";
+let p2 = "Caio";
+let p3 = "Arthur";
+let p4 = "Sarah";
+let p5 = "Thais";
+let p6 = "Vih tube";
+let p7 = "Camila";
+let p8 = "Carla";
+let p9 = "Fiuk";
+let p10 = "Gilberto";
+let p11 = "João";
+let p12 = "pocah";
+
+const num1 = 1;
+const num2 = 2;
+
+
+let res1 = prompt(`Digite ${num1} para ${p1} ou ${num2} para ${p2}`)
+if (res1 == num1) {
+    alert(p1 + " foi escolhido")
+} else {
+    alert(p2 + " foi escolhido")
+}
+let res2 = prompt(`Digite ${num1} para ${p3} ou ${num2} para ${p4}`)
+if (res2 == num1) {
+    alert(p3 + " foi escolhido")
+} else {
+    alert(p4 + " foi escolhido")
+}
+let res3 = prompt(`Digite ${num1} para ${p5} ou ${num2} para ${p6}`)
+if (res3 == num1) {
+    alert(p5 + " foi escolhido")
+} else {
+    alert(p6 + " foi escolhido")
+}
+let res4 = prompt(`Digite ${num1} para ${p7} ou ${num2} para ${p8}`)
+if (res4 == num1) {
+    alert(p7 + " foi escolhido")
+} else {
+    alert(p8 + " foi escolhido")
+}
+let res5 = prompt(`Digite ${num1} para ${p9} ou ${num2} para ${p10}`)
+if (res5 == num1) {
+    alert(p9 + " foi escolhido")
+} else {
+    alert(p10 + " foi escolhido")
+}
+let res6 = prompt(`Digite ${num1} para ${p11} ou ${num2} para ${p12}`)
+if (res6 == num1) {
+    alert(p11 + " foi escolhido")
+} else {
+    alert(p12 + " foi escolhido")
+}
